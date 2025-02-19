e35d04a56c2db2bb3f7e2c12f3df77465d9783fe
arpankumbhani <arpankumbhani@gmail.com>
2025-02-19 12:10:35 +0530

    sec commit

--- Routes/User.js
+++ Routes/User.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const { login, signin } = require("../Controllers/Auth");
-// const { verifyToken } = require("../middleware/authmiddleware");
 
 router.post("/login", login);
 router.post("/signin", signin);
